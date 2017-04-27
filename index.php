95645df02aa9c7607bf9dd3e99c04997e0982565
Pablo Varela <pablovarela182@gmail.com>
2017-04-27 15:26:57 -0400

    Pretty bootstrap

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/flatly/bootstrap.min.css" />
+    <link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/paper/bootstrap.min.css" rel="stylesheet" integrity="sha384-awusxf8AUojygHf2+joICySzB780jVvQaVCAt1clU3QsyAitLGul28Qxb2r1e5g+" crossorigin="anonymous">
     <title>Live Demo of Google Maps Geocoding Example with PHP</title>
     <style type="text/css">
         .boxy {
@@ -27,8 +27,12 @@
         <div class="row boxy">
             <div class="col-md-6 col-md-offset-3">
                 <form action="" method="post">
-                    <input type="text" name="address" placeholder="Enter any address" />
-                    <button type="submit" class="btn-primary">Geocode</button>
+                    <div class="input-group input-group-lg">
+                        <input class="form-control" type="text" name="address" placeholder="Enter any address" />
+                        <span class="input-group-btn">
+                            <button type="submit" class="btn btn-primary">Search</button>
+                        </span>
+                    </div>
                 </form>
             </div>
         </div>
@@ -44,20 +48,15 @@ if ($_POST && array_key_exists('address', $_POST)) {
     ?>
     <div class="container">
         <div class="row boxy">
-            <div class="col-md-6 col-md-offset-3">
-                <div class="row">
-                    <div class="col-md-2">Address:</div>
-                    <div class="col-md-10"><?=$formattedAddress?></div>
-                </div>
-                <div class="row">
-                    <div class="col-md-2">Latitude:</div>
-                    <div class="col-md-10"><?=$latitude?></div>
-                </div>
-                <div class="row">
-                    <div class="col-md-2">Longitude:</div>
-                    <div class="col-md-10"><?=$longitude?></div>
-                </div>
-            </div>
+            <div class="col-md-8 col-md-offset-2">
+                <dl class="dl-horizontal">
+                    <dt>Address</dt>
+                    <dd><?=$formattedAddress?><dd>
+                    <dt>Latitude</dt>
+                    <dd><?=$latitude?><dd>
+                    <dt>Longitude</dt>
+                    <dd><?=$longitude?><dd>
+                </dl>
         </div>
     </div>
     <?php
