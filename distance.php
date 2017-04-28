98040ad7be19fdd95419812fc6280cd3abc3880c
Pablo Varela <pablovarela182@gmail.com>
2017-04-27 20:14:09 -0400

    Don't show empty

--- distance.php
+++ distance.php
@@ -32,20 +32,22 @@ if ($_POST && array_key_exists('origin', $_POST) && array_key_exists('destinatio
     $origin = $data['origin'];
     $destination = $data['destination'];
     $distance = $data['distance'];
-    ?>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <dl class="dl-horizontal">
-                    <dt>From</dt>
-                    <dd><?=$origin?><dd>
-                    <dt>To</dt>
-                    <dd><?=$destination?><dd>
-                    <dt>Distance</dt>
-                    <dd><?=$distance?><dd>
-                </dl>
+    if ($data) {
+        ?>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <dl class="dl-horizontal">
+                        <dt>From</dt>
+                        <dd><?=$origin?><dd>
+                        <dt>To</dt>
+                        <dd><?=$destination?><dd>
+                        <dt>Distance</dt>
+                        <dd><?=$distance?><dd>
+                    </dl>
+            </div>
         </div>
-    </div>
-    <?php
+        <?
+    }
 }
 ?>
